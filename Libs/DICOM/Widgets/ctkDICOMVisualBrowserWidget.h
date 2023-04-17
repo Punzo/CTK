e5e9bae2512e04e9606a183fa99a936805b3868e
Davide Punzo <punzodavide@hotmail.it>
2023-04-17 14:36:52 +0200

    ENH: add import from folder

--- Libs/DICOM/Widgets/ctkDICOMVisualBrowserWidget.h
+++ Libs/DICOM/Widgets/ctkDICOMVisualBrowserWidget.h
@@ -35,6 +35,7 @@
 
 class ctkDICOMVisualBrowserWidgetPrivate;
 class ctkDICOMDatabase;
+class ctkFileDialog;
 class ctkDICOMServer;
 class ctkDICOMTaskResults;
 
@@ -42,6 +43,10 @@ class ctkDICOMTaskResults;
 class CTK_DICOM_WIDGETS_EXPORT ctkDICOMVisualBrowserWidget : public QWidget
 {
   Q_OBJECT;
+  Q_ENUMS(ImportDirectoryMode)
+  Q_PROPERTY(QString databaseDirectory READ databaseDirectory WRITE setDatabaseDirectory)
+  Q_PROPERTY(QString databaseDirectorySettingsKey READ databaseDirectorySettingsKey WRITE setDatabaseDirectorySettingsKey)
+  Q_PROPERTY(QString databaseDirectoryBase READ databaseDirectoryBase WRITE setDatabaseDirectoryBase)
   Q_PROPERTY(QString filteringPatientID READ filteringPatientID WRITE setFilteringPatientID);
   Q_PROPERTY(QString filteringPatientName READ filteringPatientName WRITE setFilteringPatientName);
   Q_PROPERTY(int numberOfSeriesPerRow READ numberOfSeriesPerRow WRITE setNumberOfSeriesPerRow);
@@ -52,9 +57,28 @@ public:
   explicit ctkDICOMVisualBrowserWidget(QWidget* parent = nullptr);
   virtual ~ctkDICOMVisualBrowserWidget();
 
+  /// Directory being used to store the dicom database
+  QString databaseDirectory() const;
+
+  /// Get settings key used to store DatabaseDirectory in application settings.
+  QString databaseDirectorySettingsKey() const;
+
+  /// Set settings key that stores DatabaseDirectory in application settings.
+  /// Calling this method sets DatabaseDirectory from current value stored in the settings
+  /// (overwriting current value of DatabaseDirectory).
+  void setDatabaseDirectorySettingsKey(const QString& settingsKey);
+
+  /// Get the directory that will be used as a basis if databaseDirectory is specified with a relative path.
+  /// @see setDatabaseDirectoryBase, setDatabaseDirectory
+  QString databaseDirectoryBase() const;
+
+  /// Set the directory that will be used as a basis if databaseDirectory is specified with a relative path.
+  /// If DatabaseDirectoryBase is empty (by default it is) then the current working directory is used as a basis.
+  /// @see databaseDirectoryBase, setDatabaseDirectory
+  void setDatabaseDirectoryBase(const QString& base);
+
   /// Dicom Database
   Q_INVOKABLE ctkDICOMDatabase* dicomDatabase()const;
-  Q_INVOKABLE void setDicomDatabase(ctkDICOMDatabase& dicomDatabase);
 
   /// See ctkDICOMDatabase for description - these accessors
   /// delegate to the corresponding routines of the internal
@@ -114,24 +138,113 @@ public:
   Q_INVOKABLE void addPatientItemWidget(const QString& patientItem);
   Q_INVOKABLE void removePatientItemWidget(const QString& patientItem);
 
+  /// Accessors to status of last directory import operation
+  int patientsAddedDuringImport();
+  int studiesAddedDuringImport();
+  int seriesAddedDuringImport();
+  int instancesAddedDuringImport();
+
+  /// Set counters of imported patients, studies, series, instances to zero.
+  void resetItemsAddedDuringImportCounters();
+
+  enum ImportDirectoryMode
+  {
+    ImportDirectoryCopy = 0,
+    ImportDirectoryAddLink
+  };
+
+  /// \brief Get value of ImportDirectoryMode settings.
+  ///
+  /// \sa setImportDirectoryMode(ctkDICOMBrowser::ImportDirectoryMode)
+  ImportDirectoryMode importDirectoryMode()const;
+
+  /// \brief Return instance of import dialog.
+  ///
+  /// \internal
+  Q_INVOKABLE ctkFileDialog* importDialog()const;
+
 public Q_SLOTS:
+  /// \brief Set value of ImportDirectoryMode settings.
+  ///
+  /// Setting the value will update the comboBox found at the bottom
+  /// of the import dialog.
+  ///
+  /// \sa importDirectoryMode()
+  void setImportDirectoryMode(ctkDICOMVisualBrowserWidget::ImportDirectoryMode mode);
+
+  void setDatabaseDirectory(const QString& directory);
+
+  /// \brief Pop-up file dialog allowing to select and import one or multiple
+  /// DICOM directories.
+  ///
+  /// The dialog is extended with two additional controls:
+  ///
+  /// * **ImportDirectoryMode** combox: Allow user to select "Add Link" or "Copy" mode.
+  ///   Associated settings is stored using key `DICOM/ImportDirectoryMode`.
+  void openImportDialog();
+
+  /// \brief Import directories
+  ///
+  /// This can be used to externally trigger an import (i.e. for testing or to support drag-and-drop)
+  ///
+  /// By default, \a mode is ImportDirectoryMode::ImportDirectoryAddLink is set.
+  ///
+  /// \sa importDirectory(QString directory, int mode)
+  void importDirectories(QStringList directories, ctkDICOMVisualBrowserWidget::ImportDirectoryMode mode = ImportDirectoryAddLink);
+
+  /// \brief Import a directory
+  ///
+  /// This can be used to externally trigger an import (i.e. for testing or to support drag-and-drop)
+  ///
+  /// By default, \a mode is ImportDirectoryMode::ImportDirectoryAddLink is set.
+  void importDirectory(QString directory, ctkDICOMVisualBrowserWidget::ImportDirectoryMode mode = ImportDirectoryAddLink);
+
+  /// \brief Import a list of files
+  ///
+  /// This can be used to externally trigger an import (i.e. for testing or to support drag-and-drop)
+  ///
+  /// By default, \a mode is ImportDirectoryMode::ImportDirectoryAddLink is set.
+  void importFiles(const QStringList& files, ctkDICOMVisualBrowserWidget::ImportDirectoryMode mode = ImportDirectoryAddLink);
+
+  /// Wait for all import operations to complete.
+  /// Number of imported patients, studies, series, images since the last resetItemsAddedDuringImportCounters
+  /// can be retrieved by calling patientsAddedDuringImport(), studiesAddedDuringImport(), seriesAddedDuringImport(),
+  ///  instancesAddedDuringImport() methods.
+  void waitForImportFinished();
+
+  /// \deprecated importDirectory() should be used
+  void onImportDirectory(QString directory, ctkDICOMVisualBrowserWidget::ImportDirectoryMode mode = ImportDirectoryAddLink);
+
+  /// slots to capture status updates from the database during an
+  /// import operation
+  void onIndexingProgress(int);
+  void onIndexingProgressStep(const QString&);
+  void onIndexingProgressDetail(const QString&);
+  void onIndexingComplete(int patientsAdded, int studiesAdded, int seriesAdded, int imagesAdded);
+
   void onFilteringPatientIDChanged();
   void onFilteringPatientNameChanged();
   void onFilteringStudyDescriptionChanged();
   void onFilteringSeriesDescriptionChanged();
   void onFilteringModalityCheckableComboBoxChanged();
   void onFilteringDateComboBoxChanged(int);
-  void onQueryPatient();
+  void onQueryPatient(bool forceFiltersEmpty = false);
   void updateGUIFromPoolManager(ctkDICOMTaskResults*);
   void onPatientItemChanged(int);
   void onClose();
   void onLoad();
+  void onImport();
+  void onStop();
 
 Q_SIGNALS:
+  /// Emitted when directory is changed
+  void databaseDirectoryChanged(const QString&);
   /// Emitted when retrieveSeries finish to retrieve the series.
   void seriesRetrieved(const QStringList& seriesInstanceUIDs);
   /// Emitted when user requested network send. String list contains list of files to be exported.
   void sendRequested(const QStringList&);
+  /// Emitted when the directory import operation has completed
+  void directoryImported();
 
 protected:
   void closeEvent(QCloseEvent *);
@@ -157,6 +270,15 @@ protected:
   void exportSeries(QString dirPath, QStringList uids);
 
 protected Q_SLOTS:
+  /// \brief Import directories
+  ///
+  /// This is used when user selected one or multiple
+  /// directories from the Import Dialog.
+  ///
+  /// \sa importDirectories(QString directory, int mode)
+  void onImportDirectoriesSelected(QStringList directories);
+  void onImportDirectoryComboBoxCurrentIndexChanged(int index);
+
   /// Called when a right mouse click is made on a patient tab widget
   void showPatientContextMenu(const QPoint& point);
   /// Called when a right mouse click is made in the studies table
